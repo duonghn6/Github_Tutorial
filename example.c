2fe9c4e3693c29bcd3b9b36487a219aaab6bc3cf
Duonghn7 <nghiaduongql@gmail.com>
2021-09-25 23:05:03 +0700

    Duonghn7: Github Tutorial new file C

---
+++ example.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello world\n");
+    int a, b, c;
+    printf("Nhap input: ");
+    scanf("%d %d", a, b);
+    printf("Tong %d + %d = %d", c);
+    return 0;
+}
